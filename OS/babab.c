9fd2e51b24ca6a04c3a8add09e0f57eef6d3d97a
student11a <ilia_2002@abv.bg>
2014-03-06 17:34:00 +0200

    g

--- OS/babab.c
+++ OS/babab.c
@@ -7,27 +7,28 @@ pthread_mutex_t sum_mutex;
 
 
 void* sum(void* arg){
-	int i;
+	int i=0;
 	
 	while(gold_in_mine>0){
 		pthread_mutex_lock(&sum_mutex);
-		printf("worker #%d entered the mine\n"),(int arg));
-		mine -= 10;
-		i+=10;
-		pthread_mutex_unlock(&sum_mutex);
-		printf("worker #%d exit the mine\n"),(int arg));
-		
+		if (gold_in_mine>0){
+			printf("worker #%d entered the mine\n",(int)arg);
+			//sleep(1);
+			gold_in_mine -= 10;
+			i+=10;
+			pthread_mutex_unlock(&sum_mutex);
+			printf("worker #%d exit the mine\n",(int)arg);
 	}
+	pthread_mutex_unlock(&sum_mutex);
 
-
-	pthread_exit(i);
+	pthread_exit((void*)i);
 
 }
 
 
 
 int main(){
-	pthread [NUM_THREADS] threads;
+	pthread[NUM_THREADS] threads;
 	
 	pthread_mutex_itit(&sum_mutex,NULL);
 	
@@ -42,7 +43,7 @@ int main(){
 	
 	printf("%d\n",common);
 
-	pthread_,utex_destroy(&sum_mutex);
+	pthread_mutex_destroy(&sum_mutex);
 	
 	return 0;
 }
